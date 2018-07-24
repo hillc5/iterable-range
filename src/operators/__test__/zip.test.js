57483d800e513e23e6ab86aaf1ce83dd2e5af247
Charlie Hill <charles.a.hill1@gmail.com>
2018-07-24 00:06:16 -0700

    Adding zip functionality.  Pulling in regenerator-runtime for generators Adding tests for zip Adding tests for isIterable util method

---
+++ src/operators/__test__/zip.test.js
@@ -0,0 +1,54 @@
+import test from 'tape';
+import { isIterable } from '../../utils';
+import zip from '../zip';
+
+test('zip - should throw an error if any given argument is not an iterable', t => {
+    const iter1 = [1, 2, 3, 4];
+    const iter2 = 'Charlie is Iterable';
+    const nonIter = 42;
+
+    t.throws(() => { zip(iter1, iter2, nonIter) }, new Error('zip requires that all arguments implement the iterable protocol.  See [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols]'));
+    t.end();
+});
+
+test('zip - should return an iterator when called with iterables', t => {
+    const iter1 = [1, 2, 3, 4];
+    const iter2 = 'Charlie is Iterable';
+
+    const z = zip(iter1, iter2);
+
+    t.equals(isIterable(z), true);
+    t.end();
+});
+
+test('zip - should interleave values from 2 iterators', t => {
+    const iter1 = [1, 2, 3, 4];
+    const iter2 = [4, 3, 2, 1];
+
+    const expected = [1, 4, 2, 3, 3, 2, 4, 1];
+
+    t.isEquivalent([...zip(iter1, iter2)], expected);
+    t.end();
+});
+
+test('zip - should iterleave values from 2 iterators until one ends, then just use the remaining iterables values', t => {
+    const iter1 = [1, 2, 3, 4, 6, 6, 6];
+    const iter2 = [4, 3, 2, 1];
+
+    const expected = [1, 4, 2, 3, 3, 2, 4, 1, 6, 6, 6];
+
+    t.isEquivalent([...zip(iter1, iter2)], expected);
+    t.end();
+});
+
+test('zip - should only be able to produce values one time (normal iterable behavior)', t => {
+    const iter1 = [1, 2, 3, 4];
+    const iter2 = [4, 3, 2, 1];
+
+    const expected = [1, 4, 2, 3, 3, 2, 4, 1];
+
+    const z = zip(iter1, iter2);
+    t.isEquivalent([...z], expected);
+    t.isEquivalent([...z], []);
+    t.end();
+});
---
+++ src/operators/zip.js
@@ -0,0 +1,49 @@
+import "regenerator-runtime/runtime";
+import { isIterable } from '../utils';
+
+function _getZipIterator(iters) {
+    let currentIterator = 0;
+    let terminatedIterators = 0;
+    let iterators = {};
+
+    return function* zipIterator() {
+        while(terminatedIterators < iters.length) {
+            if (!iterators[currentIterator]) {
+                iterators[currentIterator] = { iterator: iters[currentIterator][Symbol.iterator]() };
+            }
+            const { iterator, iteratorComplete } = iterators[currentIterator];
+            const { value, done } =  iterator.next();
+
+            if (done) {
+                if (!iteratorComplete) {
+                    terminatedIterators++;
+                    iterators[currentIterator].iteratorComplete = true;
+                }
+            } else {
+                yield value;
+            }
+
+            currentIterator = (currentIterator + 1) % iters.length;
+        }
+    }
+}
+
+/**
+ * zip returns an iterable that will produce the interleaved values from the iterables
+ * provided.  The new zipped iterable will produce values until all provided iterables
+ * are done.
+ *
+ * The iterable returned can only be run once and cannot be "replayed".
+ * 
+ * @param  {Iterable(s)} iters Any number of iterabls
+ * @return {Iterable}          
+ */
+export default function zip(...iters) {
+    if (iters.some(iter => !isIterable(iter))) {
+        throw new TypeError('zip requires that all arguments implement the iterable protocol.  See [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols]');
+    }
+
+    return {
+        [Symbol.iterator]: _getZipIterator(iters)
+    }
+};
